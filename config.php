3e186eb7e2a5f65ae426b0a3039a4b4211f58941
Philip Wardlaw <wardlaw@ualberta.ca>
2016-02-07 17:15:53 -0700

    adding config and some data

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+	// The following variables are required by the application
+	
+	// Google API key can be optained from google developer console
+	// Giigke API key must have "Google maps javascript API" enabled
+	$googleAPIKey = "AIzaSyB-C_CEWwKx2C8THaIM4bByzx2yBaza_HM";
+	
+	// Name of database that will be used by website
+	// Database must exist in same folder as index.php
+	$databaseName = 'Venue.db';
+	
+	// Home Country
+	$homeCountry = 'Singapore';
